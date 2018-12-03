8b25e171f5559a0201d7cc2378f99a5b3b76d394
Matthias Berg-Neels <matthias.uwe.berg-neels@sap.com>
2018-12-03 12:51:28 +0100

    Turtle Example

---
+++ src/uebungen/christmas/TurtleExample.java
@@ -0,0 +1,27 @@
+package uebungen.christmas;
+
+import ch.aplu.turtle.Turtle;
+
+public class TurtleExample {
+
+    public static void main(String[] args) {
+
+        Turtle myTurtle = new Turtle();
+
+        myTurtle.forward(50);
+        myTurtle.right(90);
+        myTurtle.forward(50);
+        myTurtle.left(90);
+        myTurtle.forward(50);
+        myTurtle.right(90);
+        myTurtle.forward(50);
+        myTurtle.left(90);
+        myTurtle.forward(50);
+        myTurtle.right(90);
+        myTurtle.forward(50);
+        myTurtle.left(90);
+
+        myTurtle.hideTurtle();
+    }
+
+}
