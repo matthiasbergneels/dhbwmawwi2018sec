0f442eb9187fac602ec948293ec61f55844d429c
Matthias Berg-Neels <matthias.uwe.berg-neels@sap.com>
2019-02-19 18:01:38 +0100

    Schaltjahr Lösung

--- src/uebungen/Schaltjahr.java
+++ src/uebungen/Schaltjahr.java
@@ -4,7 +4,7 @@ public class Schaltjahr {
 
     public static void main(String[] args) {
 
-        int jahr = Integer.parseInt(args[0]);
+        int jahr = 1900; // Integer.parseInt(args[0]);
         int istSchaltjahr;
 
         istSchaltjahr = pruefeSchaltjahr(jahr);
@@ -23,23 +23,24 @@ public class Schaltjahr {
 
     private static int pruefeSchaltjahr(int jahr) {
 
+        if(!(jahr > 1500 && jahr < 3000)){
+            return 0;
+        }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-        return 0;
-
+        if(jahr % 4 == 0){
+            if(jahr % 100 == 0){
+                if(jahr % 400 == 0){
+                    return 1;
+                }else{
+                    return 2;
+                }
+            }else{
+                return 1;
+            }
+        }else{
+            return 2;
+        }
     }
-
 }
 
